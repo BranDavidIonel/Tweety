55117000836481518e99a79db12512915a14efe2
Bran David Ionel <bran.david96@yahoo.com>
2020-11-19 20:04:41 +0200

    first commit

---
+++ app/Http/Controllers/TweetsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tweet;
+class TweetsController extends Controller
+{
+    public function store()
+    {
+        $attributes = request()->validate([
+            'body' => 'required|max:255'
+        ]);
+
+        Tweet::create([
+            'user_id' => auth()->id(),
+            'body' => $attributes['body']   
+        ]);
+
+        return redirect('/home');
+    }
+}
