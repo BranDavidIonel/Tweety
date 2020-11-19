7c81e37dccf0233513316b873bd2c0b82ccc35b1
Bran David Ionel <bran.david96@yahoo.com>
2020-11-19 21:40:22 +0200

    I build a following

--- app/User.php
+++ app/User.php
@@ -49,4 +49,19 @@ class User extends Authenticatable
         return Tweet::where('user_id', $this->id)->latest()->get();
         //return Tweet::all();
     }
+    
+ public function follow(User $user)
+ {
+     return $this->follows()->save($user);
+ }
+
+ public function follows()
+ {
+     return $this->belongsToMany(
+         User::class,
+         'follows',
+         'user_id', 
+         'following_user_id'
+     );
+ }
 }
